3c0824c9c47ab86530dc0fa2498f90a7d3508163
Adrien Castagliola <adrien.castagliola@gmail.com>
2021-01-19 19:57:49 +0100

    test: add user model testing

--- app/models/user/user.test.ts
+++ app/models/user/user.test.ts
@@ -1,7 +1,64 @@
+import { getSnapshot } from "mobx-state-tree"
 import { UserModel } from "./user"
+
+jest.mock("aws-amplify", () => {
+  return {
+    Auth: {
+      signIn: jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          attributes: { given_name: "Jean", family_name: "Terroire", email: "jean@terroire" },
+        })
+      }),
+      signUp: jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          user: { username: "Jean" },
+        })
+      }),
+      signOut: jest.fn().mockImplementation(() => {
+        return Promise.resolve()
+      }),
+    },
+  }
+})
 
 test("can be created", () => {
   const instance = UserModel.create({})
+  expect(getSnapshot(instance)).toMatchSnapshot()
+})
 
-  expect(instance).toBeTruthy()
+test("view", () => {
+  const instance = UserModel.create({
+    firstname: "Adrien",
+    lastname: "Castagliola",
+  })
+  expect(instance.fullName).toBe("Adrien Castagliola")
+})
+
+test("signIn", async () => {
+  const instance = UserModel.create({})
+  await instance.signIn("adrien.castagliola@gmail.com", "passw0rd")
+
+  expect(getSnapshot(instance)).toMatchSnapshot()
+})
+test("signUp", async () => {
+  const instance = UserModel.create({})
+  await instance.signUp({
+    username: "test",
+    password: "test",
+    attributes: {
+      family_name: "test",
+      given_name: "test",
+    },
+  })
+
+  expect(getSnapshot(instance)).toMatchSnapshot()
+})
+test("signOut", async () => {
+  const instance = UserModel.create({
+    firstname: "Adrien",
+    lastname: "Castagliola",
+  })
+  await instance.signOut()
+
+  expect(getSnapshot(instance)).toMatchSnapshot()
 })
