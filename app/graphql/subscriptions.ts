587547ae8dcb8199e1b5eafbc360e986a6704f20
Adrien Castagliola <adrien.castagliola@gmail.com>
2021-01-20 20:04:58 +0100

    feat: add aws graphql api config

---
+++ app/graphql/subscriptions.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateRecipe = /* GraphQL */ `
+  subscription OnCreateRecipe {
+    onCreateRecipe {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`
+export const onUpdateRecipe = /* GraphQL */ `
+  subscription OnUpdateRecipe {
+    onUpdateRecipe {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`
+export const onDeleteRecipe = /* GraphQL */ `
+  subscription OnDeleteRecipe {
+    onDeleteRecipe {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`
